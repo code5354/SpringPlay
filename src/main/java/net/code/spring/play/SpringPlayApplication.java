27a74a99d4f6e96130347a49f440ce6517ef5b7a
code5354 <stuff@theyeungs.net>
2016-12-10 18:45:00 +0000

    Added Faker to generate data

--- src/main/java/net/code/spring/play/SpringPlayApplication.java
+++ src/main/java/net/code/spring/play/SpringPlayApplication.java
@@ -1,13 +1,31 @@
 package net.code.spring.play;
 
+import com.github.javafaker.Faker;
+import net.code.spring.play.person.Person;
+import net.code.spring.play.person.PersonRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringPlayApplication {
 
+	private final Faker faker = new Faker();
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringPlayApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initializeDb(PersonRepository repository){
+		return (args) -> {
+			repository.deleteAll();
+			//Insert some random pies
+			for(int i = 0; i < 20; i++) {
+				repository.save(new Person(faker.lorem().word(), faker.lorem().word()));
+			}
+		};
 	}
 
 }
