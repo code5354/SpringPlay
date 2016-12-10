d5e3f899ee785a7413c0bc1effaf623f2357b215
code5354 <stuff@theyeungs.net>
2016-12-10 18:55:59 +0000

    Added Swagger UI

--- src/main/java/net/code/spring/play/SpringPlayApplication.java
+++ src/main/java/net/code/spring/play/SpringPlayApplication.java
@@ -7,8 +7,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringPlayApplication {
 
 	private final Faker faker = new Faker();
@@ -17,6 +23,8 @@ public class SpringPlayApplication {
 		SpringApplication.run(SpringPlayApplication.class, args);
 	}
 
+
+	// Generate fake data at startup
 	@Bean
 	public CommandLineRunner initializeDb(PersonRepository repository){
 		return (args) -> {
@@ -28,4 +36,15 @@ public class SpringPlayApplication {
 		};
 	}
 
+
+	// Enable Swagger
+	@Bean
+	public Docket swaggerSettings() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build()
+				.pathMapping("/");
+	}
 }
